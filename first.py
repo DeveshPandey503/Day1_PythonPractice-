43233da933b4ed40293ddd826390aa3ffe25f71c
DeveshPandey503 <pandeydevesh503@gmail.com>
2025-08-29 16:19:56 +0530

    Added first.py

---
+++ first.py
@@ -0,0 +1,43 @@
+print("hello world")
+# This is also a method of list
+list1 = ["apple"]
+list2 = ['ball']
+list1.extend(list2)
+print(list1)
+# name=input("Enter your name:")
+# print(f"hii,{name}")
+name="Devesh is a good boy"
+print(name.find(" "))
+letters="Dear Devesh ,\n\tyou are amazing person,\nyour nature are sensior"
+print(letters)
+friends=["Apple","Orage","Devesh","Salu",1,3,343.04]
+print(friends)
+friends[0]="Pandey"
+print(friends[0])
+print(friends[0:5])
+friends.append("kanha")
+print(friends)
+friends.reverse()
+if(5>6):
+    print("true")
+else:
+    print("false")
+print("rest of the code")
+from numpy import *
+n=array([[1,2,3,4,5],
+        [6,7,8,9,10],
+        [11,12,13,14,15]])
+print("original array")
+print(n[0])
+str1="hello \n\tDevesh ,\nyou are the best compteetor for your class mates,"
+print(str1)
+str2="dvesh pandey from nagod"
+print(len(str2))
+from array import *
+# stu_roll=array('i',[1,2,3,4,5])
+# n=len(stu_roll)
+# i=0
+# while(i<n):
+#    print(stu_roll)
+# i=i+1
+# stu_roll.append(6)
